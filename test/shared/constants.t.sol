554ee9b2ca14b1b9f46d80a873ccc83f6e4c7645
Enj <99920180+0xenj@users.noreply.github.com>
2023-12-09 21:56:12 +0100

    improve constants

--- test/shared/constants.t.sol
+++ test/shared/constants.t.sol
@@ -9,7 +9,7 @@ contract Constants {
     address constant ADMIN = address(42_000);
 
     uint256 DEFAULT_BLOCKS_COUNT = 10;
-    uint256 BUY_PACK_PRICE_MATIC = 1 ether;
-    uint256 BUY_PACK_PRICE_TOKEN = 1000;
-    uint256 EXPLORATION_COOLDOWN = 4 hours;
+    uint256 BUY_PACK_PRICE_MATIC = 5 wei;
+    uint256 BUY_PACK_PRICE_TOKEN = 10 * (10 ** 18);
+    uint256 EXPLORATION_COOLDOWN = 1 minutes;
 }
