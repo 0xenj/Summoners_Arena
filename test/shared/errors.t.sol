fe0f7796240b0d8379fce7b77b5130e31a6b8520
Enj <99920180+0xenj@users.noreply.github.com>
2023-12-09 21:57:04 +0100

    improve errors

--- test/shared/errors.t.sol
+++ test/shared/errors.t.sol
@@ -13,30 +13,7 @@ contract Errors is DSTest {
     // Add a revert error to the enum of errors.
     enum RevertStatus {
         Success,
-        SkipValidation,
-        InvalidSignature,
-        CallerNotListingOwnerOrModerator,
-        ListingNotActive,
-        CallerNotSeller,
-        CallerNotAdmin,
-        CallerNotModerator,
-        CallerNotTreasury,
-        TransferFailed,
-        ContractTokenNotSupported,
-        ListingSold,
-        SellPriceAboveZero,
-        SellPriceAboveZeroOrInvalidListing,
-        TokenBlacklisted,
-        UserBlacklisted,
-        SellerBlacklisted,
-        BuyerBlacklisted,
-        OnlyRenounceRolesForSelf,
-        ContractIsNotInitializing,
-        ContractIsInitializing,
-        ContractAlreasyInitialized,
-        Erc721InvalidTokenId,
-        Erc20InsufficientAllowance,
-        OverUnderflow
+        
     }
 
     // Associate your error with a revert message and add it to the mapping.
@@ -44,64 +21,7 @@ contract Errors is DSTest {
         _errors[
             RevertStatus.InvalidSignature
         ] = "SimpleNftMarketplace: invalid signature";
-        _errors[
-            RevertStatus.CallerNotListingOwnerOrModerator
-        ] = "SimpleNftMarketplace: Only listing owner or moderator";
-        _errors[
-            RevertStatus.CallerNotSeller
-        ] = "SimpleNftMarketplace: Only seller";
-        _errors[
-            RevertStatus.ListingNotActive
-        ] = "SimpleNftMarketplace: Listing is not active";
-        _errors[RevertStatus.CallerNotAdmin] = "Controlable: Only admin";
-        _errors[
-            RevertStatus.CallerNotModerator
-        ] = "Controlable: Only moderator";
-        _errors[RevertStatus.CallerNotTreasury] = "Controlable: Only treasury";
-        _errors[RevertStatus.TransferFailed] = "Controlable: Transfer failed";
-        _errors[
-            RevertStatus.ContractTokenNotSupported
-        ] = "ListingManager: Contract token is not supported";
-        _errors[
-            RevertStatus.ListingSold
-        ] = "ListingManager: Listing already sold";
-        _errors[
-            RevertStatus.SellPriceAboveZero
-        ] = "ListingManager: Sell price must be above zero";
-        _errors[
-            RevertStatus.SellPriceAboveZeroOrInvalidListing
-        ] = "ListingManager: Sell price must be above zero or listing does not exist";
-        _errors[
-            RevertStatus.TokenBlacklisted
-        ] = "ListingManager: Contract token is blacklisted";
-        _errors[
-            RevertStatus.UserBlacklisted
-        ] = "ListingManager: User is blacklisted";
-        _errors[
-            RevertStatus.SellerBlacklisted
-        ] = "ListingManager: Seller is blacklisted";
-        _errors[
-            RevertStatus.BuyerBlacklisted
-        ] = "ListingManager: Buyer is blacklisted";
-        _errors[
-            RevertStatus.OnlyRenounceRolesForSelf
-        ] = "AccessControl: can only renounce roles for self";
-        _errors[
-            RevertStatus.ContractIsNotInitializing
-        ] = "Initializable: contract is not initializing";
-        _errors[
-            RevertStatus.ContractIsInitializing
-        ] = "Initializable: contract is initializing";
-        _errors[
-            RevertStatus.ContractAlreasyInitialized
-        ] = "Initializable: contract is already initialized";
-        _errors[
-            RevertStatus.InvalidSignature
-        ] = "ValidateSignature: invalid signature";
-        _errors[RevertStatus.Erc721InvalidTokenId] = "ERC721: invalid token ID";
-        _errors[
-            RevertStatus.Erc20InsufficientAllowance
-        ] = "ERC20: insufficient allowance";
+        
     }
 
     // Return the error message associated with the error.
